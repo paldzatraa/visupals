8ce7f90aa509ce8097d5fc4c73f047017bd674d9
naufalds <paldzatraa@gmail.com>
2026-03-02 04:30:30 +0700

    feat: add temporary admin creation route

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,20 @@ Route::get('/', [FrontController::class, 'index'])->name('home');
 
 // Rute untuk melihat detail portofolio (berdasarkan slug/judul)
 Route::get('/portfolio/{slug}', [FrontController::class, 'show'])->name('portfolio.show');
+
+Route::get('/create-admin-secure', function () {
+    // Cek apakah user sudah ada agar tidak duplikat
+    $email = 'admin@visupals.my.id';
+    
+    if (User::where('email', $email)->exists()) {
+        return "User admin sudah ada!";
+    }
+
+    User::create([
+        'name' => 'Naufal Dzaky S',
+        'email' => $email,
+        'password' => Hash::make('imcrook122'),
+    ]);
+
+    return "Admin berhasil dibuat. Silakan login ke /admin";
+});
