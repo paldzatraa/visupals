bf193cbf55ff22bf9938814ec1a29f2f83d25b3b
naufalds <paldzatraa@gmail.com>
2026-03-02 05:03:38 +0700

    Change title and added favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Naufal Dzaky S - Portfolio</title>
+    <title>VISUPALS - Portfolio</title>
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
     <script src="https://cdn.tailwindcss.com"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <script>
